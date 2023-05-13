f873e2a2a35b1b5b54225ae296870a2a6f710309
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2023-05-13 10:05:39 +0900

    add pattern1

--- src/pattern1/main.cpp
+++ src/pattern1/main.cpp
@@ -1,11 +1,22 @@
 
 #include <memory>
 #include <iostream>
+#include "sheet.h"
 
-//using namespace design_pattern;
+using namespace design_pattern;
 
 int main(int argc, char* argv[])
 {
+  Sheet sheet;
+  sheet.setCellText(0, 0, "My angel mei.");
+  sheet.setCellText(0, 1, "Hello, World");
+  sheet.setCellText(0, 2, "straylight");
+  sheet.disp();
+  sheet.undo();
+  sheet.disp();
+  sheet.redo();
+  sheet.disp();
+
   return 0;
 }
 
